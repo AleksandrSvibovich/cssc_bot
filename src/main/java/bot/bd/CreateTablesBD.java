421c42b74b7fd898dc457ced7a3484460c6fde89
AleksandrSvibovich <fox_alex5303@yahoo.com>
2025-03-05 15:38:01 +0400

    - added spring and Postgre

--- src/main/java/bot/bd/CreateTablesBD.java
+++ src/main/java/bot/bd/CreateTablesBD.java
@@ -1,10 +1,7 @@
 package bot.bd;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.*;
-import java.util.Properties;
+
 
 public class CreateTablesBD {
 
